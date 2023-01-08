6a3ffd4e4539d19d2ae315ef3cbae46ca4296f43
Krunoslav Husak <kruno@husak.me>
2023-01-08 23:48:07 +0100

    Struct services intended for use with controllers

--- api/api.go
+++ api/api.go
@@ -14,26 +14,32 @@ import (
 )
 
 type API struct {
-	config *config.Config
+	config   *config.Config
+	server   *echo.Echo
+	services *Services
+}
+
+type Services struct {
 	db     *db.Database
 	logger *log.Logger
-	server *echo.Echo
 }
 
 func NewAPI(config *config.Config, db *db.Database, logger *log.Logger) *API {
 	return &API{
 		config: config,
-		db:     db,
-		logger: logger,
 		server: echo.New(),
+		services: &Services{
+			db:     db,
+			logger: logger,
+		},
 	}
 }
 
 func (api *API) Start() {
-	api.logger.Println("Starting server on :8080")
+	api.services.logger.Println("Starting server on :8080")
 	go func() {
 		if err := api.server.Start(":8080"); err != nil && err != http.ErrServerClosed {
-			api.logger.Fatal("Error starting server: " + err.Error())
+			api.services.logger.Fatal("Error starting server: " + err.Error())
 		}
 	}()
 }
@@ -45,6 +51,6 @@ func (api *API) WaitForShutdown() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := api.server.Shutdown(ctx); err != nil {
-		api.logger.Fatal(err)
+		api.services.logger.Fatal(err)
 	}
 }
