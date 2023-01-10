272f9baef60ce8cd6842e3f629fb408fc4a7a470
Krunoslav Husak <kruno@husak.me>
2023-01-10 16:27:25 +0100

    Code cleanup (files renamed)

--- api/models/links.go
+++ api/models/links.go
@@ -2,10 +2,18 @@ package models
 
 import (
 	"context"
+	"time"
 
 	"github.com/h00s-go/tiny-link-backend/api/services"
 	"github.com/h00s-go/tiny-link-backend/db/sql"
 )
+
+type Link struct {
+	ID        int64     `json:"id"`
+	ShortURI  string    `json:"short_uri"`
+	URL       string    `json:"url"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func GetLinkByID(s *services.Services, id string) (*Link, error) {
 	l := &Link{}
