0c8f47e577ccfc25cf3a783f531e8eb591503493
Krunoslav Husak <kruno@husak.me>
2023-02-19 23:50:29 +0100

    Renamed functions in Limiter middleware

--- api/middleware/limiter.go
+++ api/middleware/limiter.go
@@ -22,17 +22,17 @@ func (l *LimiterMiddleware) LimiterMiddleware() func(*fiber.Ctx) error {
 	return limiter.New(limiter.Config{
 		Max:          l.config.Max,
 		Expiration:   time.Duration(l.config.Expiration) * time.Second,
-		Next:         l.throttling,
-		LimitReached: l.throttleClient,
+		Next:         l.skipLimiter,
+		LimitReached: l.limitClient,
 	})
 }
 
-func (l *LimiterMiddleware) throttling(c *fiber.Ctx) bool {
+func (l *LimiterMiddleware) skipLimiter(c *fiber.Ctx) bool {
 	return c.Method() != "POST"
 }
 
-func (l *LimiterMiddleware) throttleClient(c *fiber.Ctx) error {
-	GetServices(c).Logger.Println("Throttling client: " + c.IP())
+func (l *LimiterMiddleware) limitClient(c *fiber.Ctx) error {
+	GetServices(c).Logger.Println("Limiting client: " + c.IP())
 	return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
 		"error": "Too many requests",
 	})
